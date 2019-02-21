d7caaeca286b2853a1269fab4a823ea4fb47a93c
Rodrigo Ferraz Souza <42501669+Eteciano@users.noreply.github.com>
2019-02-21 00:02:57 -0300

    Add files via upload

---
+++ Criptografia do ratao.bat
@@ -0,0 +1,143 @@
+@echo off
+color 0a
+mode 97,56
+:MENU
+title Criptografia do ratao
+echo				
+echo					      ,.                 .,
+echo					       ,: ':.    .,.    .:' :,
+echo					       ,',   '.:'   ':.'   ,',
+echo					       : '.  '         '  .' :
+echo					       ', : '           ' : ,'
+echo					       '.' .,:,.   .,:,. '.'
+echo					        ,:    V '. .' V    :,
+echo					       ,:        / '        :,
+echo					       ,:                   :,
+echo					        ,:       =:=       :,
+echo					         ,: ,     :     , :,
+echo					          :' ',.,' ',.,:' ':
+echo					         :'      ':WW::'   '.
+echo					        .:'       '::::'   ':
+echo					        ,:        '::::'    :,
+echo					        :'         ':::'    ':
+echo					       ,:           ':''     :.
+echo					      .:'             '.     ',.
+echo					     ,:'               ''     '.
+echo					     .:'               .',    ':
+echo					    .:'               .'.,     :
+echo					    .:                .,''     :
+echo					    ::                .,''    ,:
+echo					    ::              .,'','   .:'
+echo					  .,::'.           .,','     ::::.
+echo					.:'     ',.       ,:,       ,WWWWW,
+echo					:'        :       :W:'     :WWWWWWW,          .,.
+echo					:         ',      WWW      WWWWWWWWW          '::,
+echo					'.         ',     WWW     :WWWWWWWWW            '::,
+echo					 '.         :     WWW     :WWWWWWWW'             :::
+echo					  '.       ,:     WWW     :WWWWWWW'             .:::
+echo					   '.     .W:     WWW     :WWWWWW'           .,:::'
+echo					    '.   :WW:     WWW     :WWWWW'      .,,:::::''
+echo					   .,'   ''::     :W:     :WWWWW.  .,::::''
+echo					,'        ''','',',','','''WWWWW::::''
+echo					 ':,,,,,,,':  :  : : :  :  :WWWW'''
+echo 					  esconda bem seus ratos
+echo  ÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍ 
+echo  õõõõõõõõõõõõõõõõõõõõõõõõõõõõõõõõõõõõõõõõõõõõõõõõõõõõõõõõõõõõõõõõõõõõõõõõõõõõõõõõõõõõõõõõõ 
+echo  ººººººººººººººººººººººººººººººººººººººººººººººººººººººººººººººººººººººººººººººººººººººººº 
+echo  ºººººººººººÚÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄ¿ ÚÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄ¿ººººººººº 
+echo  ººººººººººº³   Criptografa - 1   ³ ³         By Rodrigo Ferraz Souza          ³ººººººººº 
+echo  ºººººººººººÀÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÙ ÀÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÙººººººººº 
+echo  ºººººººººººÚÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄ¿ ÚÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄ¿ººººººººº 
+echo  ººººººººººº³     Criptografia do ratao                ³ ³ 2 - afargotpircseD  ³ººººººººº 
+echo  ºººººººººººÀÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÙ ÀÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÙººººººººº 
+echo  ººººººººººººººººººººººººººººººººººººººººººººººººººººººººººººººººººººººººººººººººººººººººº 
+echo  õõõõõõõõõõõõõõõõõõõõõõõõõõõõõõõõõõõõõõõõõõõõõõõõõõõõõõõõõõõõõõõõõõõõõõõõõõõõõõõõõõõõõõõõõ 
+echo  ÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍ 
+echo. 
+set /p esc= oque quer fazer?
+if %esc% equ 1 goto 1
+if %esc% equ 2 goto 2
+:1
+set /p opt2=Digite seu texto 
+FOR /F "tokens=*" %%A IN ("%opt2%") DO ( 
+set s=%%A 
+) 
+set s=%s: =- % 
+set s=%s:a=2/1 % 
+set s=%s:B=2/2 % 
+set s=%s:c=2/3 % 
+set s=%s:d=3/1 % 
+set s=%s:e=3/2 % 
+set s=%s:f=3/3 % 
+set s=%s:g=4/1 % 
+set s=%s:h=4/2 % 
+set s=%s:i=4/3 % 
+set s=%s:j=5/1 % 
+set s=%s:k=5/2 % 
+set s=%s:l=5/3 % 
+set s=%s:m=6/1 % 
+set s=%s:n=6/2 % 
+set s=%s:o=6/3 % 
+set s=%s:p=7/1 % 
+set s=%s:q=7/2 % 
+set s=%s:r=7/3 % 
+set s=%s:s=7/4 % 
+set s=%s:t=8/1 % 
+set s=%s:u=8/2 % 
+set s=%s:v=8/3 % 
+set s=%s:w=9/1 % 
+set s=%s:x=9/2 % 
+set s=%s:y=9/3 % 
+set s=%s:z=9/4 % 
+set s=%s:!=00100001 % 
+set s=%s:,=00101100 % 
+set s=%s:.=00101110 % 
+set s=%s:@=00010111 % 
+set s=%s:?=00111110 % 
+set s=%s::=001110 % 
+echo %s% > TEXTO_CRIPTOGRAFADO.txt 
+start TEXTO_CRIPTOGRAFADO.txt 
+echo   
+exit
+:2
+set /p opt2= Cole o texto  
+FOR /F "tokens=*" %%B IN ("%opt2%") DO ( 
+set s=%%B 
+) 
+set s=%s:- = % 
+set s=%s:2/1 =a% 
+set s=%s:2/2 =b% 
+set s=%s:2/3 =c% 
+set s=%s:3/1 =d% 
+set s=%s:3/2 =e% 
+set s=%s:3/3 =f% 
+set s=%s:4/1 =g% 
+set s=%s:4/2 =h% 
+set s=%s:4/3 =i% 
+set s=%s:5/1 =j% 
+set s=%s:5/2 =k% 
+set s=%s:5/3 =l% 
+set s=%s:6/1 =m% 
+set s=%s:6/2 =n% 
+set s=%s:6/3 =o% 
+set s=%s:7/1 =p% 
+set s=%s:7/2 =q% 
+set s=%s:7/3 =r% 
+set s=%s:7/4 =s% 
+set s=%s:8/1 =t% 
+set s=%s:8/2 =u% 
+set s=%s:8/3 =v% 
+set s=%s:9/1 =w% 
+set s=%s:9/2 =x% 
+set s=%s:9/3 =y% 
+set s=%s:9/4 =z% 
+set s=%s:00100001 =!% 
+set s=%s:00101100 =,% 
+set s=%s:00101110 =.% 
+set s=%s:00010111 =@% 
+set s=%s:00111110 =?% 
+set s=%s:001110 =:% 
+echo %s% > TEXTO_DESCRIPTOGRAFADO.txt
+start TEXTO_DESCRIPTOGRAFADO.txt
+echo   
+exit
